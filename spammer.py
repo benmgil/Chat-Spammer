97c3d481fb43617dcbfd28f2eb0626850fede518
benmgil <gilman.benm@gmail.com>
2018-03-20 19:25:43 -0400

    Added ability to spam contents of clipboard

--- spammer.py
+++ spammer.py
@@ -4,7 +4,7 @@ import webbrowser
 import time
 
 target = input("Which chat do you want to spam? (Leave this blank if you want to manually open the chat yourself)  ")
-message = input("What message do you want to keep sending?  ")
+message = input("What message do you want to keep sending? (Leave this blank if you want to paste your clipboard)  ")
 repeats = int(input("How many times do you want to send the message?  "))
 delay = int(input("How many milliseconds do you want to wait in between each message?  "))
 
@@ -18,8 +18,13 @@ time.sleep(5)
 
 
 for i in range(0,repeats):         #Message sending loop
-	pyautogui.typewrite(message)     
-	pyautogui.press("enter")
+	if message != "":
+		pyautogui.typewrite(message)     
+		pyautogui.press("enter")
+	else:
+		pyautogui.hotkey('ctrl', 'v')      
+		pyautogui.press("enter")
+
 	time.sleep(delay/1000)
 
 
